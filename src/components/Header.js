a14aa68d64a11004734ffd8ef036e95dc7d9ebc6
IronYR <yrehan67@gmail.com>
2023-12-29 22:23:04 +0300

    e

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
-
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LogoutButton from "@/components/buttons/LogoutButton";
-import {faLink} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {getServerSession} from "next-auth";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
 export default async function Header() {
@@ -12,29 +11,27 @@ export default async function Header() {
     <header className="bg-white border-b py-4">
       <div className="max-w-4xl flex justify-between mx-auto px-6">
         <div className="flex items-center gap-6">
-          <Link href={'/'} className="flex items-center gap-2 text-blue-500">
+          <Link href={"/"} className="flex items-center gap-2 text-blue-500">
             <FontAwesomeIcon icon={faLink} className="text-blue-500" />
-            <span className="font-bold">LinkList</span>
+            <span className="font-bold">AscendLink</span>
           </Link>
           <nav className="flex items-center gap-4 text-slate-500 text-sm">
-            <Link href={'/about'}>About</Link>
-            <Link href={'/pricing'}>Pricing</Link>
-            <Link href={'/contact'}>Contact</Link>
+            <Link href={"/about"}>About</Link>
+            <Link href={"/pricing"}>Pricing</Link>
+            <Link href={"/contact"}>Contact</Link>
           </nav>
         </div>
         <nav className="flex items-center gap-4 text-sm text-slate-500">
           {!!session && (
             <>
-              <Link href={'/account'}>
-                Hello, {session?.user?.name}
-              </Link>
+              <Link href={"/account"}>Hello, {session?.user?.name}</Link>
               <LogoutButton />
             </>
           )}
           {!session && (
             <>
-              <Link href={'/login'}>Sign In</Link>
-              <Link href={'/login'}>Create Account</Link>
+              <Link href={"/login"}>Sign In</Link>
+              <Link href={"/login"}>Create Account</Link>
             </>
           )}
         </nav>
